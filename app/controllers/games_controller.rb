ab7a8bef20a370396c319c7c2b24aff5101316bd
Alexandre Gros <xelaflash@gmail.com>
2018-02-12 17:30:08 +0100

    exo working but ugly

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,20 @@
+class GamesController < ApplicationController
+  def new
+    @grid = Array.new(7) { ("A".."Z").to_a.sample }
+  end
+
+
+  def score
+    @attempt = params[:word]
+    @word_split = @attempt.upcase.chars
+    @token_grid = params[:token].chars
+    reject_array = @word_split.reject { |l| @token_grid.include? l }
+    if reject_array.empty?
+      @results = "congratulations your word is #{@attempt}"
+    else
+      @results = "Sorry but #{@attempt} can't be built from #{@token_grid.join(", ")}"
+    end
+
+  end
+
+end
